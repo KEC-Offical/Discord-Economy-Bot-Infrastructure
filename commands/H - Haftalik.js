d106dd5f51554033144cb8d17b453386af2b4ce1
KEC <79267651+KEC-Developer@users.noreply.github.com>
2022-02-10 17:09:50 +0300

    Create H - Haftalik.js

---
+++ commands/H - Haftalik.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+const ms = require("parse-ms");
+
+module.exports.run = async (bot, message, args) => {
+  let user = message.author;
+  let timeout = 604800000;
+  let amount = 5000;
+  let kecyoutube3 = "PARA BİRİMİ"
+
+  let weekly = await db.fetch(`weekly_${message.guild.id}_${user.id}`);
+
+  if (weekly !== null && timeout - (Date.now() - weekly) > 0) {
+    let time = ms(timeout - (Date.now() - weekly));
+message.channel.send(`:scroll: **Ekonomi**\n\n📟 **Olamaz!**\n\n:arrow_right: Maalesef bu haftaki günlük ${kecyoutube3} Coinini almışşsın. Lütfen ${time.hours} saat ${time.minutes} dakika ${time.seconds} saniye sonra tekrar haftalık KEC Coinini almayı dene..`);
+  } else {
+message.channel.send(`:scroll: **Ekonomi**\n\n:tada: **Tebrikler!**\n\n:arrow_right: Haftalık ${kecyoutube3} Coinin olan ${amount} ${kecyoutube3} Coin cüzdanına eklendi..`);
+    db.add(`money_${message.guild.id}_${user.id}`, amount);
+    db.set(`weekly_${message.guild.id}_${user.id}`, Date.now());
+  }
+};
+
+module.exports.help = {
+  name: "haftalık",
+};
