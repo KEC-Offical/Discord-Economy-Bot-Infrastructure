445f5c66b91457bebfbd92431edbbe2fa7b5f91a
KEC <79267651+KEC-Developer@users.noreply.github.com>
2022-02-10 17:08:29 +0300

    Create H - Gunluk.js

---
+++ commands/H - Gunluk.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+const ms = require("parse-ms");
+
+module.exports.run = async (bot, message, args) => {
+  let user = message.author;
+  let timeout = 86400000;
+  let kecyoutube2 = "PARA BİRİMİ"
+  let amount = 1000;
+  let daily = await db.fetch(`daily_${message.guild.id}_${user.id}`);
+  if (daily !== null && timeout - (Date.now() - daily) > 0) {
+    let t = ms(timeout - (Date.now() - daily));
+    message.channel.send(`:scroll: **Ekonomi**\n\n📟 **Olamaz!**\n\n:arrow_right: Maalesef bugünki günlük ${kecyoutube2} Coinini almışşsın. Lütfen ${t.hours} saat ${t.minutes} dakika ${t.seconds} saniye sonra tekrar günlük KEC Coinini almayı dene..`);
+  } else {
+    message.channel.send(`:scroll: **Ekonomi**\n\n:tada: **Tebrikler!**\n\n:arrow_right: Günlük ${kecyoutube2} Coinin olan ${amount} ${kecyoutube2} Coin cüzdanına eklendi..`);
+    db.add(`money_${message.guild.id}_${user.id}`, amount);
+    db.set(`daily_${message.guild.id}_${user.id}`, Date.now());
+  }
+};
+
+module.exports.help = {
+  name: "günlük"
+};
