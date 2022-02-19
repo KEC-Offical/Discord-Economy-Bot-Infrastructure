d9b58326b51d4a4817fea18de1b19059eca50a02
KEC <79267651+KEC-Developer@users.noreply.github.com>
2022-02-19 16:37:55 +0300

    Update H - Calis.js

--- commands/H - Calis.js
+++ commands/H - Calis.js
@@ -4,7 +4,7 @@ const ms = require("parse-ms");
 
 module.exports.run = async (bot, message, args) => {
   let user = message.author;
-  let kecyoutube1 = "PARA BİRİMİ"
+  let kecyoutube1 = "COİN BİRİMİ"
   let timeout = 3600000;
   let amount = 500;
   let daily = await db.fetch(`daily_${message.guild.id}_${user.id}`);
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
     let t = ms(timeout - (Date.now() - daily));
     message.channel.send(`:scroll: **Ekonomi**\n\n📟 **Olamaz!**\n\n:arrow_right: Fazlasıyla çalışmışsın. Lütfen ${t.hours} saat ${t.minutes} dakika ${t.seconds} saniye sonra tekrar çalışmayı dene..`);
   } else {
-    message.channel.send(`:scroll: **Ekonomi**\n\n:tada: **Tebrikler!**\n\n:arrow_right: İşinde çalıştın ve ${amount} ${kecyoutube1} cüzdanına maaş olarak eklendi..`);
+    message.channel.send(`:scroll: **Ekonomi**\n\n:tada: **Tebrikler!**\n\n:arrow_right: İşinde çalıştın ve ${amount} ${kecyoutube1} Coin cüzdanına maaş olarak eklendi..`);
     db.add(`money_${message.guild.id}_${user.id}`, amount);
     db.set(`daily_${message.guild.id}_${user.id}`, Date.now());
   }
