cb7f48eb0fc8efa013ca639a883d6d7180bc78f2
KEC <79267651+KEC-Developer@users.noreply.github.com>
2022-02-19 16:37:34 +0300

    Update H - Cuzdan.js

--- commands/H - Cuzdan.js
+++ commands/H - Cuzdan.js
@@ -6,8 +6,9 @@ module.exports.run = async (bot, message, args, utils) => {
   let bal = db.fetch(`money_${message.guild.id}_${user.id}`);
   if (bal === null) bal = 0;
     let kec = "``"
-    let kecyoutube = "PARA BİRİMİ"
- message.channel.send(`:scroll: **Ekonomi**\n\n💠 **Cüzdan**\n\n:small_orange_diamond: Cüzdanında tam olarak **__${bal}__** ${kecyoutube} Coin var..\n\n💠 **KEC Coine Mi İhtiyacın Var?**\n\n:arrow_right: ${kec}KEC!günlük${kec} Komutuyla günlük KEC Coinini alabilir..\n\n:arrow_right: ${kec}KEC!haftalık${kec} Komutuyla haftalık KEC Coinini alabilir..\n\n:arrow_right: ${kec}KEC!çalış${kec} Komutuyla çalışarak KEC Coin kazanabilirsin..`);
+    let kec1 = "PREFİX"
+    let kecyoutube = "COİN BİRİMİ"
+ message.channel.send(`:scroll: **Ekonomi**\n\n💠 **Cüzdan**\n\n:small_orange_diamond: Cüzdanında tam olarak **__${bal}__** ${kecyoutube} Coin var..\n\n💠 **${kecyoutube} Coine Mi İhtiyacın Var?**\n\n:arrow_right: ${kec}${kec1}günlük${kec} Komutuyla günlük ${kecyoutube} Coinini alabilir..\n\n:arrow_right: ${kec}${kec1}haftalık${kec} Komutuyla haftalık ${kecyoutube} Coinini alabilir..\n\n:arrow_right: ${kec}${kec1}çalış${kec} Komutuyla çalışarak ${kecyoutube} Coin kazanabilirsin..`);
 };
 
 module.exports.help = {
