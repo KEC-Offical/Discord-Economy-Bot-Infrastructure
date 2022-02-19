5c154794eda042a42cb0241b2d434d9ce4cc9ecc
KEC <79267651+KEC-Developer@users.noreply.github.com>
2022-02-19 16:36:49 +0300

    Update H - Haftalik.js

--- commands/H - Haftalik.js
+++ commands/H - Haftalik.js
@@ -6,13 +6,13 @@ module.exports.run = async (bot, message, args) => {
   let user = message.author;
   let timeout = 604800000;
   let amount = 5000;
-  let kecyoutube3 = "PARA BİRİMİ"
+  let kecyoutube3 = "COİN BİRİMİ"
 
   let weekly = await db.fetch(`weekly_${message.guild.id}_${user.id}`);
 
   if (weekly !== null && timeout - (Date.now() - weekly) > 0) {
     let time = ms(timeout - (Date.now() - weekly));
-message.channel.send(`:scroll: **Ekonomi**\n\n📟 **Olamaz!**\n\n:arrow_right: Maalesef bu haftaki günlük ${kecyoutube3} Coinini almışşsın. Lütfen ${time.hours} saat ${time.minutes} dakika ${time.seconds} saniye sonra tekrar haftalık KEC Coinini almayı dene..`);
+message.channel.send(`:scroll: **Ekonomi**\n\n📟 **Olamaz!**\n\n:arrow_right: Maalesef bu haftaki haftalık ${kecyoutube3} Coinini almışşsın. Lütfen ${time.hours} saat ${time.minutes} dakika ${time.seconds} saniye sonra tekrar haftalık ${kecyoutube3} Coinini almayı dene..`);
   } else {
 message.channel.send(`:scroll: **Ekonomi**\n\n:tada: **Tebrikler!**\n\n:arrow_right: Haftalık ${kecyoutube3} Coinin olan ${amount} ${kecyoutube3} Coin cüzdanına eklendi..`);
     db.add(`money_${message.guild.id}_${user.id}`, amount);
